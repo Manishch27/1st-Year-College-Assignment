5e7d047b770886735c4c051084307141b0cea6b8
Manish Kumar <manish.2101290@sanskriti.ac.in>
2022-03-01 14:45:47 +0530

    College Assignment with C Programming

---
+++ Program2.c
@@ -0,0 +1,22 @@
+//  Program to calculate simple interest and compound interest values with user input
+# include<stdio.h>
+# include<math.h>
+
+void main(){
+    float p, r, t, a,n, si, ci;
+	printf("Enter Principle= ");
+	scanf("%f",&p);
+	printf("Enter Rate= ");
+	scanf("%f",&r);
+	printf("Enter Time= ");
+	scanf("%f",&t);
+    printf("Enter number of times interest applied in a year = ");
+    scanf("%f",&n);
+
+	si=(p*r*t)/100;
+	printf("Simple Interest= Rs %2f",si);  // Simple interest Calculated here, Now we will calculate Compound interest
+
+    a = p*(pow(1+r/(100*n),n*t));
+    printf("Compound Interest = Rs %.2f",a);
+}
+
