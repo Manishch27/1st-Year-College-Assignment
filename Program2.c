2f325a99c3f752832cfbdd3bd1fd37ea4bd63a89
Manish Kumar <manishchaudhary2705@gmail.com>
2022-03-01 15:07:02 +0530

    Update Program2.c

--- Program2.c
+++ Program2.c
@@ -10,11 +10,11 @@ void main(){
 	scanf("%f",&r);
 	printf("Enter Time= ");
 	scanf("%f",&t);
-    printf("Enter number of times interest applied in a year = ");
+    printf("Enter number of times interest applied in a year = \n");
     scanf("%f",&n);
 
 	si=(p*r*t)/100;
-	printf("Simple Interest= Rs %2f",si);  // Simple interest Calculated here, Now we will calculate Compound interest
+	printf("Simple Interest= Rs %.2f",si);  // Simple interest Calculated here, Now we will calculate Compound interest
 
     ci = p*(pow(1+r/(100*n),n*t));
     printf("Compound Interest = Rs %.2f",a);
