855b6209d8c495286b08d02eb60b202af64588e5
Manish Kumar <manish.2101290@sanskriti.ac.in>
2022-03-03 22:38:03 +0530

    College Assignment with C Programming

---
+++ Pattern_Program1.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main(){
+    int num, row, col;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    for(row=1; row<=num; row++){
+        for(col=1; col<=row; col++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
